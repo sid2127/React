1d8de2ccf265067b1b5fb25dc021914750039a5c
sid2127 <mehraprema8@gmail.com>
2025-10-10 23:32:54 +0530

    Created Chai.jsx and learn how to import and export

---
+++ 01-Basic_react/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello ! My Name Is Siddhart Singh</h1>
+    <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ 01-Basic_react/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai() {     //function name should start with capital letter (like here Chai not chai)
+    return (
+        <>
+        <h2>Chai aur Code</h2>
+        </>
+    )
+}
+
+export default Chai
+
